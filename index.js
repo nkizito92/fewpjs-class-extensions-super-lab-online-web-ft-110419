68a71d0201fed62156141c822a64073cd801a211
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 01:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ class Tree {
 }
 
 class Deciduous extends Tree {
-  constructor(name) {
+  constructor(species, name) {
     super(species);
     this.name = name;
   }
   
   static definition() {
-    return Tree.definition() + "Deciduous trees shed their leaves annually."
+    return super.definition() + " Deciduous trees shed their leaves annually."
   }
+}
+
+class Evergreen extends Tree {
+   constructor(species, name) {
+    super(species);
+    this.name = name;
+   }
+   
+   static definition() {
+     return super.definition() + " " + "Evergreens keep their leaves all year round."
+   }
 }
