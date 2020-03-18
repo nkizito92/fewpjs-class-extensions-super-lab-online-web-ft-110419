6a92e0fb1d7bd7c6b7a9d4dc0b835332fb280e37
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 01:51:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your classes here
+class Tree {
+  constructor(species) {
+    this.species = species
+  }
+  
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves." 
+  }
+}
+
+class Deciduous extends Tree {
+  constructor(name) {
+    super(species);
+    this.name = name;
+  }
+  
+  static definition() {
+    return Tree.definition() + "Deciduous trees shed their leaves annually."
+  }
+}
